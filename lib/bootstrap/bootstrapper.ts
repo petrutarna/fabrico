34a13c876e74075e24268c55a411c880bac3d211
Nicola Gallo <nicolagmt@hotmail.com>
2018-10-30 16:12:25 +0000

    Started implementation of the cli project

--- lib/bootstrap/bootstrapper.ts
+++ lib/bootstrap/bootstrapper.ts
@@ -3,13 +3,10 @@ import { Container } from 'inversify';
 
 // modules
 import { IBootstrapper } from '../core/bootstrap/bootstrapper.interface';
-import { DI_TYPES } from './di-types';
-import { IProject, Project, } from '../projects';
 
 class Bootstrapper implements IBootstrapper {
 
   public onInit(container: Container): void {
-    container.bind<Project>(DI_TYPES.Project).to(Project).inSingletonScope();
   }
 
 }
--- lib/projects/project.ts
+++ lib/projects/project.ts
@@ -1,8 +1,5 @@
 // libs
 import { injectable } from 'inversify';
-import * as fs from 'fs-extra';
-const yamlJs = require('js-yaml');
-const path = require('path');
 
 // modules
 import { Metadata } from './model';
@@ -12,19 +9,7 @@ import { IProject } from './project.interface';
 export class Project implements IProject {
 
   async saveMetaData(force: boolean, workingPath: string, metaData: Metadata): Promise<void> {
-    const yaml = yamlJs.safeDump(metaData);
-    const filePath = path.join(workingPath, '.fabrico.yml');
-    const fileExist = await fs.pathExists(filePath);
-    if (fileExist) {
-      if (force) {
-        await fs.remove(filePath);
-      } else {
-        throw new Error(`File already exist (${filePath}).`);
-      }
-    } else {
-      await fs.createFile(filePath);
-    }
-    await fs.appendFile(filePath, yaml);
+    throw new Error('Method not implemented.');
   }
 
 }
